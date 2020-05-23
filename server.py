96d33a5945d3536fc63eb0b6988d1ec547039799
Dariusrussellkish <russeldk@bc.edu>
2020-05-23 13:54:38 -0400

    fixed R reset in consensus accept

--- server.py
+++ server.py
@@ -169,13 +169,13 @@ def process_message():
                 R[int(message["id"])] = 1
                 logging.info(f"Server {serverID} updating R: {R}")
 
-            if sum(R) >= int(params["servers"]) - int(params["f"]):
-                logging.info(f"Server {serverID} accepting consensus update")
-                v = float(v) / float(sum(R))
-                p += 1
-                R = list([0 for _ in range(nServers)])
-                R[serverID] = 1
-                updated = True
+                if sum(R) >= int(params["servers"]) - int(params["f"]):
+                    logging.info(f"Server {serverID} accepting consensus update")
+                    v = float(v) / float(sum(R))
+                    p += 1
+                    R = list([0 for _ in range(nServers)])
+                    R[serverID] = 1
+                    updated = True
 
             # send update to controller if we changed state
             if updated:
