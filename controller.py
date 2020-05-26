e07cb74912619474f05e0424a73d54bf3cab6335
Dariusrussellkish <russeldk@bc.edu>
2020-05-25 20:51:58 -0400

    debug algorithm 1

--- controller.py
+++ controller.py
@@ -69,9 +69,9 @@ def get_wait_time(isDown, shape=3, scale=2):
     if wait > 10:
         wait = 10
     if isDown:
-        wait = interp(wait, (0, 10), (0, 5))
+        wait = interp(wait, (0, 10), (0, 2))
     else:
-        wait = interp(wait, (0, 10), (0, 5))
+        wait = interp(wait, (0, 10), (0, 2))
     return wait
 
 
